2b1c8c3443b45a3b6f14c178c881c473ac8da9d2
LegenD742 <devansh7420@gmail.com>
2025-12-01 00:18:44 +0530

    added

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
             <div className="container mx-auto flex px-5 py-12 md:flex-row flex-col-reverse items-center">
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left  md:mb-0 items-center text-center">
                     <h1 className=" sm:text-4xl text-3xl mb-4 font-bold">
-                        Welcome To Shopping Cart </h1>
+                        Welcome To Shoppers </h1>
                     <p className="mb-8 leading-relaxed">
                         At our online store, we offer a wide range of high-quality products to meet your shopping needs. Whether you're looking for fashion, electronics, home appliances, or any other item, we have it all covered.
                     </p>
